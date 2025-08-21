59d8fef5bb48aafdaad3ebfe68ba36ae9ee1eaef
QuocBaoVu <e1375387@u.nus.edu>
2025-08-21 22:55:07 +0800

    Added List funtionality

--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.time.LocalDate;
 
@@ -9,6 +11,8 @@ public class Tuesday {
         System.out.println("Hello! I'm Tuesday");
         System.out.println("What can I do for you?");
 
+        List<String> list = new ArrayList<>();
+
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
@@ -18,10 +22,17 @@ public class Tuesday {
             if (input.equals("What the date today")) {
                 LocalDate now = LocalDate.now();
                 System.out.println(now);
-            } else {
-                System.out.println(input);
             }
-    }
+            else if (input.equals("list")) {
+                Integer counter = 1;
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            } else {
+                System.out.println("added: " + input);
+                list.add(input);
+            }
+        }
 
     }
 }

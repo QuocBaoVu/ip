f360b1449a8f0876758784163bd5f059f2189642
QuocBaoVu <e1375387@u.nus.edu>
2025-08-21 22:49:54 +0800

    Added Echo

--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+
 public class Tuesday {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Tuesday");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            if (input.equals("What the date today")) {
+                LocalDate now = LocalDate.now();
+                System.out.println(now);
+            } else {
+                System.out.println(input);
+            }
+    }
 
     }
 }

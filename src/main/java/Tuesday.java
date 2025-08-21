59b1521483b0e60d725d2b2e5bfd27884dfa8508
QuocBaoVu <e1375387@u.nus.edu>
2025-08-21 22:35:14 +0800

    Completed Level-0

---
+++ src/main/java/Tuesday.java
@@ -0,0 +1,9 @@
+public class Tuesday {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Tuesday");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
